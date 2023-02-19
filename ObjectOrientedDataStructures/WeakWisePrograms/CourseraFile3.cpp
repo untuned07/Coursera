21808d5d4deb21cf38a4becf7741915453221b07
Kartik Ginwal <114296676+untuned07@users.noreply.github.com>
2023-02-19 06:07:52 +0530

    Add files via upload

---
+++ ObjectOrientedDataStructures/WeakWisePrograms/CourseraFile3.cpp
@@ -0,0 +1,59 @@
+/* Class Pair has already been declared
+ * as shown in the following comments:
+ *
+ * class Pair {
+ * public:
+ *   int *pa,*pb;
+ *   Pair(int, int);
+ *   Pair(const Pair &);
+ *  ~Pair();
+ * };
+ *
+ * Implement its member functions below.
+ */
+
+ class Pair {
+ public:
+   int *pa,*pb;
+   Pair(int, int);
+   Pair(const Pair &);
+  ~Pair();
+ };
+ 
+ Pair::Pair(int a, int b){
+   pa = new int;
+   *pa = a;
+   pb = new int; 
+   *pb = b;
+ };
+
+ Pair::Pair(const Pair &p){
+   pa = new int;
+   *pa = *(p.pa);
+   pb = new int;
+   *pb = *(p.pb);
+ }
+
+ Pair::~Pair(){
+   delete pa;
+   pa = nullptr;
+   delete pb;
+   pb = nullptr;
+ }
+ 
+ /* Here is a main() function you can use
+  * to check your implementation of the
+  * class Pair member functions.
+  */
+  
+int main() {
+  Pair p(15,16);
+  Pair q(p);
+  Pair *hp = new Pair(23,42);
+  delete hp;
+  
+  std::cout << "If this message is printed,"
+    << " at least the program hasn't crashed yet!\n"
+    << "But you may want to print other diagnostic messages too." << std::endl;
+  return 0;
+}
